f8eccba21f5603e8e169702ffff486447a5aac46
Joe Zhou <zhoushuqun@gmail.com>
2021-08-16 04:51:51 +0800

    Add test.

--- spring-cache-redis-scored-spring-boot/spring-cache-redis-scored-spring-boot-autoconfigure/src/test/java/org/oxerr/spring/cache/redis/scored/spring/boot/autoconfigure/ScoredRedisCacheAutoConfigurationTest.java
+++ spring-cache-redis-scored-spring-boot/spring-cache-redis-scored-spring-boot-autoconfigure/src/test/java/org/oxerr/spring/cache/redis/scored/spring/boot/autoconfigure/ScoredRedisCacheAutoConfigurationTest.java
@@ -2,10 +2,14 @@ package org.oxerr.spring.cache.redis.scored.spring.boot.autoconfigure;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Duration;
+
 import org.junit.jupiter.api.Test;
+import org.oxerr.spring.cache.redis.scored.ScoredRedisCache;
 import org.oxerr.spring.cache.redis.scored.ScoredRedisCacheManager;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 class ScoredRedisCacheAutoConfigurationTest {
@@ -20,6 +24,20 @@ class ScoredRedisCacheAutoConfigurationTest {
 			.run(context -> {
 				assertThat(context).hasSingleBean(LettuceConnectionFactory.class);
 				assertThat(context).hasSingleBean(ScoredRedisCacheManager.class);
+
+				ScoredRedisCache cache = ((ScoredRedisCache) (context.getBean(CacheManager.class).getCache("hello")));
+				assertThat(cache.getCacheConfiguration().getTtl()).isEqualTo(Duration.ZERO);
+			});
+
+		this.contextRunner
+			.withBean(LettuceConnectionFactory.class)
+			.withPropertyValues("spring.cache.redis.time-to-live=11m")
+			.run(context -> {
+				assertThat(context).hasSingleBean(LettuceConnectionFactory.class);
+				assertThat(context).hasSingleBean(ScoredRedisCacheManager.class);
+
+				ScoredRedisCache cache = ((ScoredRedisCache) (context.getBean(CacheManager.class).getCache("hello")));
+				assertThat(cache.getCacheConfiguration().getTtl()).isEqualTo(Duration.ofMinutes(11));
 			});
 	}
 
