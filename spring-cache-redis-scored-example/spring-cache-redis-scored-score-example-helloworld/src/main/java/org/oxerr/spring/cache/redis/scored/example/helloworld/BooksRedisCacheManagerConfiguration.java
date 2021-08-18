9d6a65a216a878205982a0036708e8ef44e9747b
Joe Zhou <zhoushuqun@gmail.com>
2021-08-18 21:47:38 +0800

    Revise javadoc.

--- spring-cache-redis-scored-example/spring-cache-redis-scored-score-example-helloworld/src/main/java/org/oxerr/spring/cache/redis/scored/example/helloworld/BooksRedisCacheManagerConfiguration.java
+++ spring-cache-redis-scored-example/spring-cache-redis-scored-score-example-helloworld/src/main/java/org/oxerr/spring/cache/redis/scored/example/helloworld/BooksRedisCacheManagerConfiguration.java
@@ -9,6 +9,13 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
+/**
+ * Check
+ * <a href="https://docs.spring.io/spring-boot/docs/2.5.3/reference/htmlsingle/#features.caching.provider.redis">
+ * Spring Boot Documentation
+ * </a>
+ * for more about RedisCacheManagerBuilderCustomizer.
+ */
 @Configuration(proxyBeanMethods = false)
 public class BooksRedisCacheManagerConfiguration {
 
